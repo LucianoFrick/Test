f36c6ca047a566159d9b8a756379a25580de8150
castr <castrio98@gmail.com>
2020-05-10 13:24:45 +0200

    Sistemato problema in registrazione

--- app/src/main/java/com/example/prova/MainActivity.java
+++ app/src/main/java/com/example/prova/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                finish();
                 startActivity(intent);
             }
         });
